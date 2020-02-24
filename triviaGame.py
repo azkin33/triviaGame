969a0a8213519f9a058bf9fcdce3ed37aa47ba39
barisazkin <33733377+azkin33@users.noreply.github.com>
2020-02-24 14:29:58 +0300

    Update triviaGame.py

--- triviaGame.py
+++ triviaGame.py
@@ -1,14 +1,20 @@
 import os ,subprocess
 
-x = input('''Hi, type /new to start a new game \n/next to get next question \n /answer your_answer to answer \n. Y
-ou can change session by typing /id your_id. \n Type /exit to exit\n\n\n''')
+x = input('''Hi, type "/new [questionCount]" to start a new game where questionCount is a whole number. (Default questionCount is 10)
+\n/next to get next question \n /answer your_answer to answer. \n.
+You can change session by typing /id your_id. \nType /exit to exit\n\n\n''')
 
 myId=0
 
 while(x != "/exit"):
-    if x == "/new":
+    if x.split(" ")[0] == "/new":
         #os.system("curl http://localhost:8080/newGame?amount=10")
-        p = subprocess.Popen("curl http://localhost:8080/newGame?amount=10", stdout=subprocess.PIPE, shell=True)
+        reqUrl = "curl http://localhost:8080/newGame?amount=" 
+        if len(x.split(" "))==2:
+            reqUrl += str(x.split(" ")[1])
+        else:
+            reqUrl += str(10)
+        p = subprocess.Popen(reqUrl, stdout=subprocess.PIPE, shell=True)
         (output, err) = p.communicate()
         p.status = p.wait()
         myId = output.split()[-1].decode()
@@ -23,4 +29,6 @@ while(x != "/exit"):
         print("+".join(x.split(" ")[1:]))
         os.system("curl -X POST http://localhost:8080/answer?id="+str(myId)+"\&answer="+"+".join(x.split(" ")[1:]))
         print("\n\n")
+    elif x.split(" ")[0] == "/id":
+        myId = x.split(" ")[1]
     x=input()
